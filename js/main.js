550b189e3f6fb06ea4baf65c23c2d23392f72e5c
R4M45H <R4M45H@users.noreply.github.com>
2020-05-05 23:27:08 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -61,3 +61,18 @@ $('.counter__item-number').each(function() {
   });  
 });
 
+$(document).ready(function(){
+	$("#menu, #reserv-btn").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 2000);
+	});
+});
